15fb5ad467b6846fbd8ffca7f337332ebb2b9744
eckon <niklas.meyer95@hotmail.de>
2022-09-21 23:44:54 +0200

    feat(api): split api routes into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,9 @@
-use actix_web::{get, middleware, web, App, Error, HttpResponse, HttpServer};
-
+use actix_web::{middleware, web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
-use uuid::Uuid;
 
 mod db;
 mod model;
-
-#[get("/user/{user_id}")]
-async fn get_user(
-    pool: web::Data<PgPool>,
-    user_id: web::Path<Uuid>,
-) -> Result<HttpResponse, Error> {
-    let user = db::get_user(pool.as_ref(), user_id.into_inner())
-        .await
-        .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    Ok(HttpResponse::Ok().json(user))
-}
-
-#[get("/user/create/{user_name}")]
-async fn add_user(
-    pool: web::Data<PgPool>,
-    user_name: web::Path<String>,
-) -> Result<HttpResponse, Error> {
-    let user = db::create_user(pool.as_ref(), user_name.into_inner())
-        .await
-        .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    Ok(HttpResponse::Ok().json(user))
-}
+mod api;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -57,8 +31,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .wrap(middleware::Logger::default())
-            .service(add_user)
-            .service(get_user)
+            .service(api::add_user)
+            .service(api::get_user)
     })
     .bind(server_address)?
     .run()
