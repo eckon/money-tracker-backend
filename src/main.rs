2a2001da65ebca47065ebbd986fc2856f205246e
eckon <niklas.meyer95@hotmail.de>
2022-10-15 00:19:15 +0200

    feat: switch to axum

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
-use actix_web::{middleware, web, App, HttpServer};
+use std::net::SocketAddr;
+
+use axum::{Extension, Router};
 use sqlx::postgres::PgPoolOptions;
+use tower_http::trace::TraceLayer;
 
 mod api;
 mod db;
 mod model;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> () {
     dotenv::dotenv().ok();
 
-    std::env::set_var("RUST_LOG", "debug");
-    std::env::set_var("RUST_BACKTRACE", "1");
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "debug")
+    }
 
-    #[allow(clippy::expect_used)]
-    let db_url =
-        std::env::var("DATABASE_URL").expect("DATABASE_URL should be set through `.env` file");
+    tracing_subscriber::fmt::init();
 
-    #[allow(clippy::expect_used)]
+    let db_connection_str = std::env::var("DATABASE_URL").expect(".env has valid DATABASE_URL");
+
     let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&db_url.clone())
+        .connect(&db_connection_str)
         .await
-        .expect("pool should be created and connected to running database");
+        .expect("can connect to database");
 
-    let server_address = ("127.0.0.1", 3000);
-    log::info!(
-        "Starting HTTP server on {}:{}",
-        server_address.0,
-        server_address.1
-    );
+    let app = Router::new()
+        .merge(api::get_router())
+        .layer(Extension(pool))
+        .layer(TraceLayer::new_for_http());
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .wrap(middleware::Logger::default())
-            .configure(api::config)
-    })
-    .bind(server_address)?
-    .run()
-    .await
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
