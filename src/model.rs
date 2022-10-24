1bf8a0d4ba950ad3f4f89a5d11d56b14fa5318ec
eckon <niklas.meyer95@hotmail.de>
2022-10-25 00:36:59 +0200

    fix: remove unused user endpoints and implementation

--- src/model.rs
+++ src/model.rs
@@ -2,17 +2,6 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct User {
-    pub id: Uuid,
-    pub name: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct CreateUser {
-    pub name: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
 pub struct CreateAccount {
     pub name: String,
 }
