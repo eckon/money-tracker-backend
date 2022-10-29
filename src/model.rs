83fbd255b07b2e02b1ce15a84e147416b785e5b1
eckon <niklas.meyer95@hotmail.de>
2022-10-29 21:24:33 +0200

    feat: return created data for cost and payment

--- src/model.rs
+++ src/model.rs
@@ -32,11 +32,58 @@ pub struct CreateCostDto {
     pub tags: Option<Vec<String>>,
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct CostDto {
+    pub id: Uuid,
+    pub account_id: Uuid,
+    pub amount: f64,
+    pub event_date: chrono::NaiveDate,
+    pub description: Option<String>,
+    pub tags: Option<Vec<String>>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct PaymentDto {
+    pub id: Uuid,
+    pub payer_account_id: Uuid,
+    pub lender_account_id: Uuid,
+    pub amount: f64,
+    pub event_date: chrono::NaiveDate,
+    pub description: Option<String>,
+}
+
+
 impl From<Account> for AccountDto {
     fn from(account: Account) -> Self {
         Self {
             id: account.id,
             name: account.name,
+        }
+    }
+}
+
+impl From<Cost> for CostDto {
+    fn from(cost: Cost) -> Self {
+        Self {
+            id: cost.id,
+            tags: cost.tags,
+            amount: (cost.amount as f64) / 100.0,
+            account_id: cost.account_id,
+            event_date: cost.event_date,
+            description: cost.description,
+        }
+    }
+}
+
+impl From<Payment> for PaymentDto {
+    fn from(payment: Payment) -> Self {
+        Self {
+            id: payment.id,
+            amount: (payment.amount as f64) / 100.0,
+            payer_account_id: payment.payer_account_id,
+            lender_account_id: payment.lender_account_id,
+            event_date: payment.event_date,
+            description: payment.description,
         }
     }
 }
@@ -53,7 +100,7 @@ pub struct Account {
 pub struct Payment {
     pub id: Uuid,
     pub payer_account_id: Uuid,
-    pub lender_account_id: Uuid, // mayube rename to payback?
+    pub lender_account_id: Uuid,
     pub amount: i64,
     pub event_date: chrono::NaiveDate,
     pub description: Option<String>,
