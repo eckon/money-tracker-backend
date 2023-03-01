2f295e6ed899d0da5adc6e8ee3961e4e3550e93a
eckon <niklas.meyer95@hotmail.de>
2023-03-02 00:12:12 +0100

    refactor(snapshot): abstract calculation for unit testing

--- src/service/cost.rs
+++ src/service/cost.rs
@@ -266,48 +266,123 @@ pub async fn get_current_snapshot(
         let to_pay_debts = get_debts_for_account(pool, account.id).await?;
         let being_payed_debts = get_debts_of_account(pool, account.id).await?;
 
-        // calculate the overall debt from payer account to lender account
-        let mut results: HashMap<Uuid, i64> = HashMap::new();
-
-        // payer account pays to lender account via payment
-        for payment in &payed_payments {
-            *results.entry(payment.lender_account_id).or_insert(0) += payment.amount;
-        }
-
-        // lender account could have payed to payer account via payment
-        for payment in &given_payments {
-            *results.entry(payment.payer_account_id).or_insert(0) -= payment.amount;
-        }
-
-        // lender account could have debts to payer account via debts
-        for debt in &being_payed_debts {
-            *results.entry(debt.0).or_insert(0) += debt.1;
-        }
-
-        // payer account could have debts to lender account via debts
-        for debt in to_pay_debts {
-            *results.entry(debt.0).or_insert(0) -= debt.1;
-        }
-
-        for result in &results {
-            let lender_account = accounts
-                .iter()
-                .find(|acc| acc.id == *result.0)
-                .unwrap_or(account);
-
-            // lender will have their own costs (to see the general distribution of cost, so ignore them here
-            if lender_account.id == account.id {
-                continue;
-            }
-
-            all_debts.push(response::CalculatedDebtDto {
-                payer_account: account.clone().into(),
-                lender_account: lender_account.clone().into(),
-                // only transform to float at the end to not run into rounding errors
-                amount: Conversion::to_float(*result.1),
-            });
-        }
+        all_debts = accumulate_costs(
+            &payed_payments,
+            &given_payments,
+            &to_pay_debts,
+            &being_payed_debts,
+            &accounts,
+            account,
+            all_debts,
+        );
     }
 
     Ok(all_debts)
+}
+
+fn accumulate_costs(
+    payed_payments: &[entity::Payment],
+    given_payments: &[entity::Payment],
+    to_pay: &[(Uuid, i64)],
+    being_paid: &[(Uuid, i64)],
+    accounts: &[entity::Account],
+    payer_account: &entity::Account,
+    accumulated_debts: Vec<response::CalculatedDebtDto>,
+) -> Vec<response::CalculatedDebtDto> {
+    // calculate the overall debt from payer account to lender account
+    let mut results: HashMap<Uuid, i64> = HashMap::new();
+
+    // payer account pays to lender account via payment
+    for payment in payed_payments {
+        *results.entry(payment.lender_account_id).or_insert(0) += payment.amount;
+    }
+
+    // lender account could have payed to payer account via payment
+    for payment in given_payments {
+        *results.entry(payment.payer_account_id).or_insert(0) -= payment.amount;
+    }
+
+    // lender account could have debts to payer account via debts
+    for debt in being_paid {
+        *results.entry(debt.0).or_insert(0) += debt.1;
+    }
+
+    // payer account could have debts to lender account via debts
+    for debt in to_pay {
+        *results.entry(debt.0).or_insert(0) -= debt.1;
+    }
+
+    let mut accumulated_debts = accumulated_debts;
+    for result in &results {
+        let lender_account = accounts
+            .iter()
+            .find(|acc| acc.id == *result.0)
+            .unwrap_or(payer_account);
+
+        // lender will have their own costs (to see the general distribution of cost, so ignore them here
+        if lender_account.id == payer_account.id {
+            continue;
+        }
+
+        accumulated_debts.push(response::CalculatedDebtDto {
+            payer_account: payer_account.clone().into(),
+            lender_account: lender_account.clone().into(),
+            // only transform to float at the end to not run into rounding errors
+            amount: Conversion::to_float(*result.1),
+        });
+    }
+
+    accumulated_debts.clone()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correctly_calculate_current_snapshot() {
+        let lender_id = Uuid::new_v4();
+        let payer_id = Uuid::new_v4();
+
+        let lender_account = entity::Account {
+            id: lender_id,
+            name: "Lender".to_string(),
+        };
+
+        let payer_account = entity::Account {
+            id: payer_id,
+            name: "Payer".to_string(),
+        };
+
+        // payer added cost of 412
+        //   with a split of lender needs to pay 11, rest is payed by payer (and ignored - as cant ow yourself)
+        let to_pay = vec![(lender_id, 11)];
+        let being_paid = vec![(payer_id, 401)];
+
+        // lender pays back 100 to payer
+        let payed_payments = vec![entity::Payment {
+            id: Uuid::new_v4(),
+            payer_account_id: payer_id,
+            lender_account_id: lender_id,
+            amount: 100,
+            event_date: chrono::NaiveDate::from_ymd_opt(2020, 1, 1).unwrap(),
+            description: None,
+        }];
+
+        // noone pays back to lender
+        let given_payments = vec![];
+
+        let results = accumulate_costs(
+            &payed_payments,
+            &given_payments,
+            &to_pay,
+            &being_paid,
+            &[lender_account, payer_account.clone()],
+            &payer_account,
+            vec![],
+        );
+
+        // resulting in lender: 11 was needed, 100 was payed, so 89 is now owed to lender instead
+        assert_eq!(results[0].amount, 0.89);
+    }
 }
